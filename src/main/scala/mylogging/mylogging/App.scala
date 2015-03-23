da741c7c74d10fb56fa48185be33d1b7da86657e
Viviana Petrescu <petrescu.viviana@gmail.com>
2015-03-23 23:23:38 +0100

    added new line parser

--- src/main/scala/mylogging/mylogging/App.scala
+++ src/main/scala/mylogging/mylogging/App.scala
@@ -47,7 +47,7 @@ val l1 = """2015-03-10 05:15:53,196 INFO org.apache.hadoop.yarn.server.resourcem
 val l2 = """2015-03-10 05:15:53,196 INFO org.apache.hadoop.yarn.server.resourcemanager.RMAppManager$ApplicationSummary: appId=application_1425682538854_1748,name=decompress,user=joe,queue=default,state=FINISHED,trackingUrl=http://master1:8088/proxy/application_1425682538854_1748/jobhistory/job/job_1425682538854_1748,appMasterHost=icdatasrv019.icdatacluster2,startTime=1425960925158,finishTime=1425960946325,finalStatus=FAILED"""
 
 val l3 = """2015-03-03 17:59:51,137 INFO org.apache.hadoop.yarn.server.resourcemanager.RMAppManager$ApplicationSummary: appId=application_1424932957480_0341,name=Spark shell,user=bob,queue=default,state=FINISHED,trackingUrl=http://master1:8088/proxy/application_1424932957480_0341/A,appMasterHost=icdatasrv058.icdatacluster2,startTime=1425392421737,finishTime=1425401990053,finalStatus=SUCCEEDED"""
-
+val l4 = """2015-03-10 13:44:55,243 INFO org.apache.hadoop.yarn.server.nodemanager.containermanager.monitor.ContainersMonitorImpl: Memory usage of ProcessTree 227772 for container-id container_1425682538854_2409_01_000289: 2.4 GB of 8.7 GB physical memory used; 8.0 GB of 18.2 GB virtual memory used"""
 */
 
 
@@ -59,6 +59,7 @@ abstract class LogLine extends java.io.Serializable
 case class Foo(ts: String, appatt: String, state: String) extends LogLine
 case class AppSummary(timestamp: String, app: String, name: String, user: String, state:String,
   url:String, host: String, startTime: String, endTime: String, finalStatus: String) extends LogLine
+case class AppContainer(container_id: String, app:String) extends LogLine
 case class UnknownLine() extends LogLine
 
 
@@ -81,16 +82,25 @@ object LogP extends RegexParsers with java.io.Serializable {
         ~",finalStatus="~ident ^^ {
        case t~_~app~_~name~_~user~_~state~_~url~_~host~_~stime~_~etime~_~finalStatus =>
          AppSummary(t, app, name, user, state, url, host, stime, etime, finalStatus)
-    }
+       }
+   | timestamp
+        ~"INFO org.apache.hadoop.yarn.server.nodemanager.containermanager.monitor.ContainersMonitorImpl: Memory usage of ProcessTree"~processtree
+        ~"for container-id "~container_part
+        ~":"~other ^^ {
+           case t~_~processtree~_~containernbr~_~other => AppContainer(containernbr, "application"+ containernbr.drop(9).dropRight(10))
+         }
   )
 
+  val container_part = "container_[0-9]+_[0-9]+_[0-9]+_[0-9]+".r
+  val processtree: Parser[String] = "[0-9]+".r
   val ident: Parser[String] = "[A-Za-z0-9_]+".r
   val identW: Parser[String] = "[A-Za-z0-9_ ]+".r
   val timestamp: Parser[String] = "2015-[0-9][0-9]-[0-9][0-9] [0-9:,]+".r
+  val other: Parser[String] = "[a-zA-Z0-9.;]+".r
   val url: Parser[String] = "http://[a-zA-Z0-1.]+:[0-9]+/[a-zA-Z0-9_/]+".r
 }
 
-
+//container_1425682538854_2409_01_000289
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -101,14 +111,14 @@ object LogAnalysis1 {
     val sc = new SparkContext(new SparkConf().setAppName("LogAnalysis"))
    
     // sc.textFile(','.join(files))
-    val lines = sc.textFile("/Users/alex/workspace_scala/clusterlogs/yarn-yarn-resourcemanager-master1.log*")
+    val lines = sc.textFile("/Users/alex/workspace_scala/clusterlogs/yarn-yarn-*")
 
     def parseLine(l: String): LogLine =
       LogP.parse(LogP.logline, l).getOrElse(UnknownLine())
        
 
     def f(a: LogLine) = a match {
-      case AppSummary(t, app, name, user, state, url, host, stime, etime, finalStatus) => List(user+" "+state+" "+host+" "+stime+" "+etime+" "+finalStatus)
+      case AppSummary(t, app, name, user, state, url, host, stime, etime, finalStatus) => List(user+" "+app+" "+host+" "+stime+" "+etime+" "+finalStatus)
       case _ => List()
     }
     def ftemp(a: LogLine) = a match {
@@ -116,11 +126,11 @@ object LogAnalysis1 {
       case _ => List()
     }
     def fother(a: LogLine) = a match {
-      case Foo(t, app, user) => List(t+" "+app+" "+user)
+      case AppContainer(container, app) => List(container +" "+app)
       case _ => List()
     }
 
-    val ll = lines.map(l => parseLine(l)).flatMap(f).cache
+    val ll = lines.map(l => parseLine(l)).flatMap(fother).cache
     ll.saveAsTextFile("app_summaries_one")  // in the user's HDFS home directory
 
     val ll2 = lines.map(l => parseLine(l)).flatMap(ftemp).cache
@@ -128,7 +138,7 @@ object LogAnalysis1 {
     
     val svmdata = MLUtils.loadLibSVMFile(sc, "app_summaries_three/part-*")
     
-    val CVfold:Int = 5
+    val CVfold:Int = 2
     var auROC:Array[Double] = new Array[Double](CVfold)
     for (cv_iter <- 0 to CVfold - 1) {
     	val splits = svmdata.randomSplit(Array(0.6, 0.4), seed = 11L + cv_iter)
